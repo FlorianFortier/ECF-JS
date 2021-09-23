be2369591cb1f05cd405e820ab7a5257823dbda2
FlorianFortier <florian.f.imp@gmail.com>
2021-09-23 09:12:56 +0200

    requete music brainz et dom sans loader

--- JS/API.js
+++ JS/API.js
@@ -1,52 +1,131 @@
+noResult();
 form.addEventListener("submit", function (e) {
+    deleteResult();
     e.preventDefault();
-    const getResult = new XMLHttpRequest();
-    getResult.open('GET', 'https://musicbrainz.org/ws/2/recording/?inc=genres&fmt=json&limit=100&query=' + "" + encodeURIComponent(inputSearch.value) + "", true);
-    getResult.addEventListener("readystatechange", function () {
-        if (getResult.readyState === XMLHttpRequest.DONE) {
-            if (getResult.status === 200) {
-                const parseResult = JSON.parse(getResult.responseText)
-                loopOnObject(parseResult);
-            } else {
-                alert("Error")
+
+    // request title 
+
+    if (Select.value === 'Title') {
+        const getResult = new XMLHttpRequest();
+        getResult.open('GET', 'https://musicbrainz.org/ws/2/recording?fmt=json&limit=100&query=recording:' + '"' + encodeURIComponent(inputSearch.value) + '"', true);
+        getResult.addEventListener("readystatechange", function () {
+            if (getResult.readyState === XMLHttpRequest.DONE) {
+                if (getResult.status === 200) {
+                    const parseResult = JSON.parse(getResult.responseText)
+                    console.log(Select.value);
+                    // console.log(parseResult);
+                    loopOnObject(parseResult);
+
+                } else {
+                    alert("Error")
+                }
             }
-        }
-    })
-    getResult.send();
-})
-
-let nb = 0;
-
-function loopOnObject(parseResult) {
-    for (i in parseResult.recordings) {
-        nb++;
-        // 
-        let artistName = [];
-        let album = [];
-        let albumId = [];
-
-        artistId = parseResult.recordings[i].id;
-        title = parseResult.recordings[i].title;
-
-        for (k in parseResult.recordings[i]["artist-credit"]) {
-            artistName.push(parseResult.recordings[i]["artist-credit"][k].name)
-        }
-
-        // if album is undefined then set it to this
-
-        if (parseResult.recordings[i]["releases"] === undefined) {
-            album = 'undefined';
-        } else {
-            for (l in parseResult.recordings[i]["releases"]) {
-                album.push(parseResult.recordings[i]["releases"][l].title)
-                albumId.push(parseResult.recordings[i]["releases"][l].id)
-
-            }
-        }
-
-        // TODO  put parameters later like ArtistId etc.. when needed
-
-        addResult(artistName, title, album, nb);
-
+        })
+        getResult.send();
     }
-}
+
+    // request artist 
+
+    else if (Select.value === 'Artist') {
+        const getResult = new XMLHttpRequest();
+        getResult.open('GET', 'https://musicbrainz.org/ws/2/recording?fmt=json&limit=100&query=artist:' + '"' + encodeURIComponent(inputSearch.value) + '"', true);
+        getResult.addEventListener("readystatechange", function () {
+            if (getResult.readyState === XMLHttpRequest.DONE) {
+                if (getResult.status === 200) {
+                    const parseResult = JSON.parse(getResult.responseText)
+                    console.log(Select.value);
+                    // console.log(parseResult);
+                    loopOnObject(parseResult);
+
+                } else {
+                    alert("Error")
+                }
+            }
+        })
+        getResult.send();
+    }
+
+    //request Album
+
+    else if (Select.value === 'Album') {
+        const getResult = new XMLHttpRequest();
+        getResult.open('GET', 'https://musicbrainz.org/ws/2/recording?fmt=json&limit=100&query=release:' + '"' + encodeURIComponent(inputSearch.value) + '"', true);
+        getResult.addEventListener("readystatechange", function () {
+            if (getResult.readyState === XMLHttpRequest.DONE) {
+                if (getResult.status === 200) {
+                    const parseResult = JSON.parse(getResult.responseText)
+                    console.log(Select.value);
+                    // console.log(parseResult);
+                    loopOnObject(parseResult);
+
+                } else {
+                    alert("Error")
+                }
+            }
+        })
+        getResult.send();
+    }
+
+    // request everything
+
+    else if (Select.value === 'Everything') {
+        const getResult = new XMLHttpRequest();
+        getResult.open('GET', 'https://musicbrainz.org/ws/2/recording/?inc=genres&fmt=json&limit=100&query=' + "" + encodeURIComponent(inputSearch.value) + "", true);
+        getResult.addEventListener("readystatechange", function () {
+            if (getResult.readyState === XMLHttpRequest.DONE) {
+                if (getResult.status === 200) {
+                    const parseResult = JSON.parse(getResult.responseText)
+                    console.log(Select.value);
+
+                    loopOnObject(parseResult);
+                } else {
+                    alert("Error")
+                }
+            }
+        })
+        getResult.send();
+    }
+
+    if (inputSearch.value === "") {
+        noResult();
+    }
+
+    let nb = 0;
+
+    // function, that get values from the objects and push it to arrays, then we call the addResult function  
+
+    function loopOnObject(parseResult) {
+        for (i in parseResult.recordings) {
+            nb++;
+            // 
+            let artistName = [];
+            let album = [];
+            let albumId = [];
+
+            artistId = parseResult.recordings[i].id;
+            title = parseResult.recordings[i].title;
+
+            for (k in parseResult.recordings[i]["artist-credit"]) {
+                artistName.push(parseResult.recordings[i]["artist-credit"][k].name)
+            }
+
+            // if album is undefined then set it to undifined   
+
+            if (parseResult.recordings[i]["releases"] === undefined) {
+                album = 'undefined';
+            } else {
+                for (l in parseResult.recordings[i]["releases"]) {
+                    album.push(parseResult.recordings[i]["releases"][l].title)
+                    albumId.push(parseResult.recordings[i]["releases"][l].id)
+
+                }
+            }
+
+
+            addResult(artistName, title, album, nb);
+
+        }
+        calculerSommeResult(nb);
+        nb = 0;
+    }
+})
--- JS/dom.js
+++ JS/dom.js
@@ -1,49 +1,83 @@
 
+// Query Selector
+
+
+const zoneResult = document.querySelector(".result-div");
+const inputSearch = document.querySelector(".input-search");
+const form = document.querySelector(".form");
+const Select = document.querySelector("#select");
+const sommeZone = document.querySelector("result-zone");
 
 
 
-
-
-// Query
-
-
-const zoneResult = document.querySelector(".result-div")
-const inputSearch = document.querySelector(".input-search")
-const form = document.querySelector(".form")
-
-
-
+// function that create and apply  html element et define styles
 
 function addResult(artistName, title, album, nb) {
 
     // Create HTML elements
 
-    const addDiv = document.createElement('div')
-    const addPindex = document.createElement("p")
-    const addPtitle = document.createElement("p")
-    const addPalbum = document.createElement("p")
-    const addPartist = document.createElement("p")
+    const addDiv = document.createElement('div');
+    const addPindex = document.createElement("p");
+    const addPtitle = document.createElement("p");
+    const addPalbum = document.createElement("p");
+    const addPartist = document.createElement("p");
 
 
     // add css classes
-    addDiv.className = "text-result grid rangement full"
-    addPindex.className = "text"
-    addPtitle.className = "text"
-    addPalbum.className = "text"
-    addPartist.className = "text"
 
-    addPindex.textContent = nb
-    addPtitle.textContent = title
-    addPalbum.textContent = album
-    addPartist.textContent = artistName
+    addDiv.className = "text-result grid rangement full";
+    addPindex.className = "text";
+    addPtitle.className = "text";
+    addPalbum.className = "text";
+    addPartist.className = "text";
+
+    // define text content
+
+    addPindex.textContent = nb;
+    addPtitle.textContent = title;
+    addPalbum.textContent = album;
+    addPartist.textContent = artistName;
 
     // append
 
+    addDiv.appendChild(addPindex);
+    addDiv.appendChild(addPartist);
+    addDiv.appendChild(addPtitle);
+    addDiv.appendChild(addPalbum);
+    zoneResult.appendChild(addDiv);
+}
+
+// delete last results
+
+function deleteResult() {
+
+    zoneResult.innerHTML = "";
+
+}
 
 
-    addDiv.appendChild(addPindex)
-    addDiv.appendChild(addPartist)
-    addDiv.appendChild(addPtitle)
-    addDiv.appendChild(addPalbum)
-    zoneResult.appendChild(addDiv)
+// function that get the sum of search results
+
+function calculerSommeResult(nb) {
+
+    const somme = document.createElement("p");
+    somme.className = "text";
+    somme.textContent = nb + "Résultat trouvées";
+    zoneResult.prepend(somme);
+
+}
+
+// function, that show if there's no results from the response
+
+function noResult() {
+    const noTextResult = document.createElement("p");
+    noTextResult.className = "text row jtcn";
+    noTextResult.textContent = "pas de résultat de recherche";
+    zoneResult.appendChild(noTextResult);
+}
+
+function loadingAnimation() {
+    const loader = document.createElement("div");
+    zoneResult.appendChild(loader);
+    loader.className = "loader";
 }
